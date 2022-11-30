f1637ade76519bfc6197995c81aeeb0ef7f49bcb
validacijak <Lgnexus5132@gmail.com>
2022-11-30 14:14:47 +0100

    Data for Exercise 3

---
+++ Exercise_3/Exercise 01/class/Hero.js
@@ -0,0 +1,29 @@
+/**
+ @author Stefan Trajkovski <trajkovski.stefan@hotmail.com>
+ */
+
+ class Hero extends NPC {
+    damage;
+    boost = 0;
+
+    constructor(name = "HERO", hp = 100, damage = 10) {
+        super(name, hp);
+        this.damage = damage;
+    }
+
+    attack(npc) {
+        if (this.boost !== 0)
+            npc.hp = npc.hp - this.damage * (this.boost / 100);
+        else
+            npc.hp = npc.hp - this.damage;
+    }
+
+
+    get boost() {
+        return this._boost;
+    }
+
+    set boost(value) {
+        this._boost = value;
+    }
+}
---
+++ Exercise_3/Exercise 01/class/NPC.js
@@ -0,0 +1,29 @@
+/**
+ @author Stefan Trajkovski <trajkovski.stefan@hotmail.com>
+ */
+
+ class NPC {
+    name;
+    hp;
+
+    constructor(name = "NPC", hp = 100) {
+        this._name = name;
+        this._hp = hp;
+    }
+
+    status() {
+        return this.name + " - " + this.hp;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get hp() {
+        return this._hp;
+    }
+
+    set hp(value) {
+        this._hp = value;
+    }
+}
